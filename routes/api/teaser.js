b79c337e2ecbf3ef46265a715c89ebb5a035ce2b
nanonansen <buero@nanonansen.com>
2019-11-14 17:01:33 +0100

    Add Page Url to API respond.

--- routes/api/teaser.js
+++ routes/api/teaser.js
@@ -4,15 +4,15 @@ const express = require("express");
 const router = express.Router();
 
 getContent = async slug => {
-  // let url = `https://www.highsnobiety.com/p/${slug}`;
-  let url = slug;
-  let res = await axios.get(url);
-  let html = res.data;
-  const $ = await cheerio.load(html);
-  const title = $('meta[property="og:title"]').attr("content");
-  const image = $('meta[property="og:image"]').attr("content");
-  const description = $('meta[property="og:description"]').attr("content");
-  return { title: title, imgUrl: image, excerpt: description };
+    // let url = `https://www.highsnobiety.com/p/${slug}`;
+    let url = slug;
+    let res = await axios.get(url);
+    let html = res.data;
+    const $ = await cheerio.load(html);
+    const title = $('meta[property="og:title"]').attr("content");
+    const image = $('meta[property="og:image"]').attr("content");
+    const description = $('meta[property="og:description"]').attr("content");
+    return { title: title, imgUrl: image, excerpt: description, pageUrl: url };
 };
 
 // router.get("/:slug", async (req, res) => {
@@ -27,14 +27,14 @@ getContent = async slug => {
 // });
 
 router.get("/query", async (req, res) => {
-  try {
-    const slug = req.query.page;
-    const responseBody = await getContent(slug);
-    res.status(200).json(responseBody);
-  } catch (err) {
-    console.log(err.message);
-    res.status(404).json({ msg: err.message });
-  }
+    try {
+        const slug = req.query.page;
+        const responseBody = await getContent(slug);
+        res.status(200).json(responseBody);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).json({ msg: err.message });
+    }
 });
 
 module.exports = router;
